a652aaef2e1583fe6fa420f12e0868fb015d3293
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-02-12 22:04:01 +0600

    Tuples.py Set.py

---
+++ Set.py
@@ -0,0 +1,7 @@
+num1 = {1, 2, 3, 4, 5}
+num2 = set([4,5,6])
+num2.add(7)
+num2.remove(5)
+print(num2)
+print(4 not in num2)
+print(4 in num2)
---
+++ Tuples.py
@@ -0,0 +1,9 @@
+students = (
+    ("Anisul Islam",27,3.56),
+    "Saiful Islam",
+    "Nishat Sultana",
+)
+
+#students[0] = "Moinul Islam"
+print(students[0])
+print(students[1:])
