c1125e8374962bed795d5e8075cabc8b58b4d561
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-02-12 20:51:49 +0600

    number of latters

---
+++ input defiend.py
@@ -0,0 +1,10 @@
+numOfWords = 0
+numOfLetters = 0
+numOfDigits = 0
+
+text = input("Enter a text of numbers : ")
+for x in text :
+    x = x.lower()
+    if x>= 'a' and x <= 'z':
+        numOfLetters = numOfLetters+1
+print(numOfLetters)
