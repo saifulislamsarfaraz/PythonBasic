dbe20ce385f1c2035635936484ca716c646e0745
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-02-12 21:33:29 +0600

    number of latters

---
+++ Matrikx.py
@@ -0,0 +1,8 @@
+matrix = [
+    [1,2,3],
+    [4,5,6]
+]
+
+for row in matrix:
+    for col in row:
+        print(col)
--- input defiend.py
+++ input defiend.py
@@ -7,5 +7,12 @@ for x in text :
     x = x.lower()
     if x>= 'a' and x <= 'z':
         numOfLetters = numOfLetters+1
+    if x>= 0 and x<=9:
+        numOfDigits = numOfDigits + 1
+    if x==" ":
+        numOfWords = numOfWords + 1
+
 
 print(numOfLetters)
+print(numOfDigits)
+print(numOfWords)
